cebd6e84d0696bb32584e32a53f9f9a22e94f2aa
dragonfore4 <kemkaikub@gmail.com>
2025-06-24 21:43:40 +0700

    Add login page and middleware for authentication

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function middleware(request: NextRequest) {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    console.log("🧠 Middleware triggered for:", request.nextUrl.pathname);
+    console.log(request.url);
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+    return NextResponse.next();
+
+}
+
+export const config = {
+    matcher: [
+        "/"
+    ]
+}
